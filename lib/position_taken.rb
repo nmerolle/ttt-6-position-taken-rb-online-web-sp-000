e68ca395dc608f17281730309bdd5acbdaa4f394
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-23 09:19:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-    taken = nil
+    
     if board[index] == " "||board[index] == ""||board[index] == nil 
-      taken = false
+     false
     else
-      taken = true
+     true
     end
 end
   
